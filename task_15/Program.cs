f14434a98f4ce0d87591d69ea9aab0f03ecffe74
Abvorobey <abvorobey@gmail.com>
2023-02-14 23:52:17 +0300

    Выходной ли день?

---
+++ task_15/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.Clear();
+Console.WriteLine("Введите цифру:");
+int a = int.Parse(Console.ReadLine());
+while (a<1 || a>7)
+{
+    Console.WriteLine("Неправильная цифра, сколько дней в неделе?");
+    Console.WriteLine("Введите цифру:");
+    a = int.Parse(Console.ReadLine());
+}
+if (a<6)
+{
+    Console.WriteLine($"{a} -> нет");
+}
+else {Console.WriteLine($"{a} -> да");}
