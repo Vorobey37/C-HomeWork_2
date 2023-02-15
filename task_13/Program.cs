a8a519637ced9b922a42440268f5d6d04d2f9ccf
Abvorobey <abvorobey@gmail.com>
2023-02-15 22:58:09 +0300

    Третья цифра числа

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -11,3 +11,15 @@ while (a>-100 && a<100)
     Console.WriteLine($"{a} -> третьей цифры нет");
     return;
 }
+int b = a;
+int i = 2;
+while (b<-99 || b>99)
+{
+    b = b/10;
+    i++;
+}
+int n = (a%Convert.ToInt32(Math.Pow(10,(i-2))))/Convert.ToInt32(Math.Pow(10,(i-3)));
+if (a>0)
+{Console.WriteLine($"{a} -> {n}");}
+else {Console.WriteLine($"{a} -> {-n}");}
+
