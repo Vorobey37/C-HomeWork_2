804728408b0deb794f189991cd41bbeae507b9bb
Abvorobey <abvorobey@gmail.com>
2023-02-17 00:47:39 +0300

    Задача про Петю

--- bonus_task/Program.cs
+++ bonus_task/Program.cs
@@ -16,6 +16,17 @@
 
 Выходные данные
 В выходной файл OUTPUT.TXT выведите единственное целое число – номер Пети в шеренге учеников.*/
+
+
+/*Алгоритм следующий: 
+ - задаем случайное число (кол-во учеников);
+ - создаем случайный массив;
+ - сортируем значения массива по убыванию (шеренга невозрастания);
+ - задаем ещё одно случайное число (рост Пети);
+ - выводим (записываем условие задачи для случайного случая);
+ - определяем позицию Пети;
+ - выводим результат */
+
 Console.Clear();
 int N = new Random().Next(1, 101);
 Console.WriteLine(N);
@@ -23,8 +34,38 @@ int []Ai = new int[N];
 for (int i=0; i<N; i++)
 {
 Ai[i] = new Random().Next(1, 201);
-Console.Write($"{Ai[i]} ");
 }
+//Далее пузырьковая сортировка:
+for (int NN=0; NN < (N-1); NN++)
+{
+    for (int i=0; i<(N-1-NN); i++)
+    {
+        if (Ai[i]<Ai[(i+1)])
+        {
+            int buf = Ai[i];
+            Ai[i] = Ai[(i+1)];
+            Ai[(i+1)] = buf;
+        }
+    }
+}
+for (int i=0; i<N; i++)
+{
+    Console.Write($"{Ai[i]} ");
+}
+
 Console.WriteLine();
 int X = new Random().Next(1, 201);
 Console.WriteLine(X);
+
+while (X<=Ai[(N-1)])
+{
+    Console.WriteLine((N+1));
+}
+
+int ii = 0;
+while (Ai[ii]>=X)
+{
+     ii = ii+1;
+}
+Console.WriteLine((ii+1));
+
